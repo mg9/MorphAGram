3b1a9dcf9d73c4bb47ec87430d296bb2582ad1eb
francescacallejas <ffc2108@columbia.edu>
2020-02-18 17:34:50 -0500

    Added parsing and analysis methods

--- main.py
+++ main.py
@@ -132,6 +132,174 @@ def read_linguistic_knowledge(lk_file):
                 suffixes.append(line)
     return prefixes, suffixes
 
+def parse_seg(file, morphs):
+    '''
+    This function parses the output of the word morphologies into
+    a human-readable format that denotes a word split into its morphemes
+    separated by a '+' character and converting hex denoted characters
+    into their respective unicode symbol. It writes these conversions
+    into a file.
+
+    Input:
+    segmentation_file: a txt file that contains each words' morphology trees
+    morphs: a RegEx that denotes the morphemes that will be denoted in the final
+    output
+    '''
+    output = []
+
+    for line in open(file, 'r'):
+        fields = line.split('(')
+        word = ""
+        curr_morph = ""
+        stem_open = False
+        morph_change = False
+        ch_added = False
+        inner_child = []
+        # Search for a field match with a morph RegEx given as input.
+        for field in fields[1:]:
+            m = field.split()
+            # Keep track of children and current morpheme.
+            rgx = r'^' + morphs + r'(#[0-9]*)?$'
+            r = re.search(rgx, m[0])
+            if r: # RegEx of current morph was matched to input.
+                curr_morph = m[0]
+            if curr_morph is not "":
+                if m[0] != curr_morph:
+                    inner_child.append(m[0])
+                # Pop all ")".
+                if ")" in field:
+                    f = list(field)
+                    f.pop() # Pop '/n' character.
+                    while ")" in f:
+                        last_ch = f.pop()
+                        if len(inner_child):
+                            last_popped_morph = inner_child.pop()
+                        else:
+                            break
+                        if last_popped_morph == "Char":
+                            # Parse to hex value and convert.
+                            h = field.split()[1]
+                            e = h.find(")")
+                            ch = convert_hex_to_string(h[:e])
+                            # Verify if Stem "(" or ")" is necessary.
+                            if re.match("^Stem*", curr_morph) and not stem_open:
+                                stem_open = True
+                                if len(word)==0:
+                                    word += "("
+                            elif re.match("^Stem*", curr_morph) is None and stem_open:
+                                stem_open = False
+                                word += ")"
+                            # Add "+" if a change of current morpheme has occurred.
+                            # if (curr_morph != last_morph and last_morph != "") or (morph_change and not stem_open):
+                            if morph_change and ch_added:
+                                word += "+"
+                                if stem_open:
+                                    word += "("
+                                morph_change = False
+                                ch_added = False
+                            # Remove trailing new line character if necessary.
+                            if list(ch)[0] == '\x00':
+                                word += str(list(ch)[1])
+                                ch_added = True
+                            else:
+                                word += ch
+                                ch_added = True
+                            morph_change = False
+                        if last_ch == '$'or len(inner_child) == 0:
+                            if stem_open:
+                                stem_open = False
+                                word += ")"
+                            morph_change = True
+                            curr_morph = ""
+                            break
+        output.append(word)
+    return output
+
+def affix_analyzer(file, n, morphs):
+    prefix_counter = {}
+    suffix_counter = {}
+
+    pre = morphs.split("|")[0]
+    pre = pre[1:]
+
+    for line in open(file, 'r'):
+        fields = line.split('(')
+        affix = ""
+        curr_morph = ""
+        prefix = False
+        inner_child = []
+        # Search for a field match with a morph RegEx given as input.
+        for field in fields[1:]:
+            m = field.split()
+            # Keep track of children and current morpheme.
+            rgx =  r'^' + morphs + r'(#[0-9]*)?$'
+            r = re.search(rgx, m[0])
+            if r:  # RegEx of current morph was matched to input.
+                curr_morph = m[0]
+            if curr_morph is not "":
+                if re.search(pre, curr_morph):
+                    prefix = True
+                else:
+                    prefix = False
+                if m[0] != curr_morph:
+                    inner_child.append(m[0])
+                # Pop all ")".
+                if ")" in field:
+                    f = list(field)
+                    f.pop()  # Pop '/n' character.
+                    while ")" in f:
+                        last_ch = f.pop()
+                        if len(inner_child):
+                            last_popped_morph = inner_child.pop()
+                        else:
+                            curr_morph = ""
+                            break
+                        if last_popped_morph == "Char":
+                            # Parse to hex value and convert.
+                            h = field.split()[1]
+                            e = h.find(")")
+                            ch = convert_hex_to_string(h[:e])
+
+                            # Remove trailing new line character if necessary.
+                            if list(ch)[0] == '\x00':
+                                affix += str(list(ch)[1])
+                            else:
+                                affix += ch
+                        if last_ch == '$' or len(inner_child) == 0:
+                            print(affix)
+                            if prefix:
+                                if affix in prefix_counter:
+                                    prefix_counter[affix] += 1
+                                else:
+                                    prefix_counter[affix] = 1
+                            else:
+                                if affix in suffix_counter:
+                                    suffix_counter[affix] += 1
+                                else:
+                                    suffix_counter[affix] = 1
+                            affix = ""
+                            curr_morph = ""
+                            break
+    # Return highest n affixes.
+    # Sort prefixes.
+    prefix_list_sorted = sorted(prefix_counter.items(), key=lambda x: x[1], reverse=True)
+    suffix_list_sorted = sorted(suffix_counter.items(), key=lambda x: x[1], reverse=True)
+
+    n_affixes = []
+    p = 0 # index for prefix_list_sorted
+    s = 0 # index for suffix_list_sorted
+    while n > 0:
+        if prefix_list_sorted[p][1] > suffix_list_sorted[s][1]:
+            n_affixes.append(prefix_list_sorted[p][0])
+            p += 1
+        else:
+            n_affixes.append(suffix_list_sorted[s][0])
+            s += 1
+        n -= 1
+
+    return n_affixes
+
+
         
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -6,3 +6,6 @@ def convert_string_to_hex_chars(string):
         hex_char = '0'*(4-len(hex_char)) + hex_char
         hex_chars.append(hex_char)
     return ' '.join(hex_chars)
+
+def convert_hex_to_string(hex):
+    return bytes.fromhex(hex).decode()
