4e3664239d05747569e745547a09b21d2d6a8092
francescacallejas <ffc2108@columbia.edu>
2020-05-02 02:06:43 -0400

    update from utf-8 to utf-16

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 def convert_string_to_hex_chars(string):
     hex_chars = []
     for char in list(string):
-        hex_char = char.encode("utf-8").hex()
+        hex_char = char.encode("utf-16").hex()
         hex_char = '0' * (4 - len(hex_char)) + hex_char
         hex_chars.append(hex_char)
     return ' '.join(hex_chars)
 
 
 def convert_hex_to_string(hex):
-    s = bytes.fromhex(hex).decode('utf-8')
+    s = bytes.fromhex(hex).decode('utf-16')
     # Remove trailing new line character if necessary.
     if list(s)[0] == '\x00':
         return str(list(s)[1])
